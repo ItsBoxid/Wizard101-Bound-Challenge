ade693f802459d257d8de7aa62f7141257994d0b
ItsBoxid <EvanBA@pm.me>
2026-05-31 21:57:21 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
-console.log("Hello");
+const cards = [
+    "Blades I",
+    "Healer I",
+    "Cross-Trained",
+    "Spell Caster",
+    "Traps I",
+    "Shields I",
+    "Card Collector",
+    "Minion Friend"
+];
+
+const generateBtn =
+    document.getElementById("generateBtn");
+
+const cardsContainer =
+    document.getElementById("cards");
+
+generateBtn.addEventListener("click", () => {
+
+    cardsContainer.innerHTML = "";
+
+    const shuffled =
+        [...cards].sort(() => Math.random() - 0.5);
+
+    const selected =
+        shuffled.slice(0, 3);
+
+    selected.forEach(cardName => {
+
+        const card =
+            document.createElement("div");
+
+        card.classList.add("card");
+
+        card.textContent =
+            cardName;
+
+        cardsContainer.appendChild(card);
+
+    });
+
+});
