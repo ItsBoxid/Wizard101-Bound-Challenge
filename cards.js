f76121011912b751244f5f3f66876d4c42b5fd9b
ItsBoxid <EvanBA@pm.me>
2026-05-31 22:40:00 -0400

    Create cards.js

---
+++ cards.js
@@ -0,0 +1,18 @@
+const cards = [
+
+    {
+        name: "Blades I",
+        tree: "Offense"
+    },
+
+    {
+        name: "Healer I",
+        tree: "Defense"
+    },
+
+    {
+        name: "Spell Caster",
+        tree: "Gear"
+    }
+
+];
